2daffaeb59129b8e132b50b84d475356bcfb8c17
taroimocup <66675162+taroimocup@users.noreply.github.com>
2020-08-31 18:07:58 +0900

    不要なコメントの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname, uniqueness: { case_sensitive: false }
-    # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    # validates :email, format: { with: VALID_EMAIL_REGEX }
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
