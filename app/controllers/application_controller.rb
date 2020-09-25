ae6aa96057cdb161561b5ccf6ce34464ed727d48
taroimocup <66675162+taroimocup@users.noreply.github.com>
2020-09-25 11:55:49 +0900

    エラー対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  class MyLogger < Logger
+    include LoggerSilence
+    include ActiveSupport::LoggerThreadSafeLevel
+  end
+  before_action :mylogger_test
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
   
@@ -8,9 +13,19 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
   end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
+    end
+  end
+
+  def mylogger_test
+    mylogger = MyLogger.new(STDOUT)
+    mylogger.silence do
+      mylogger.debug("controller = #{controller_name}")
+      mylogger.info("action = #{action_name}")
+      mylogger.error("controler#action = #{controller_name}##{action_name}")
     end
   end
 end
